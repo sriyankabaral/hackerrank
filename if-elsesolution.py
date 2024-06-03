bbe15d648549f55cd09854ff1aad989cf89f3ac6
sriyankabaral18 <120296847+sriyankabaral@users.noreply.github.com>
2024-06-03 22:58:00 +0545

    Update if-elsesolution.py

--- if-elsesolution.py
+++ if-elsesolution.py
@@ -1,7 +1,7 @@
 n=int(input())
 if (n%2 != 0): 
  print("Weird")
-elif (n%2 == 0 and n >= 2 and n <= 5 ):
+elif (n%2 ==0 and n >= 2 and n <= 5 ):
   print("Not Weird")
 elif (n%2 == 0 and n >= 6 and n <= 20):
   print("Weird")

1809184545aca839d112c54fd63e9bf4ac34374f
sriyankabaral18 <120296847+sriyankabaral@users.noreply.github.com>
2024-06-03 22:57:38 +0545

    Update if-elsesolution.py

--- if-elsesolution.py
+++ if-elsesolution.py
@@ -5,7 +5,7 @@ elif (n%2 == 0 and n >= 2 and n <= 5 ):
   print("Not Weird")
 elif (n%2 == 0 and n >= 6 and n <= 20):
   print("Weird")
-elif (n%2 == 0 and n >= 6 and n <= 20):
+elif (n%2 ==0 and n >= 6 and n <= 20):
    print("Weird")
 else :
     print("Not Weird")
